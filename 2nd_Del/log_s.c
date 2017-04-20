3b56e7cf3dac4702d94604cdd69e6c059c0df1d6
fritzOSU03 <fritzOSU03@hotmail.com>
2017-04-20 14:33:03 -0500

    Update log_s.c

--- 2nd_Del/log_s.c
+++ 2nd_Del/log_s.c
@@ -1,9 +1,21 @@
 // File: log_s.c
 // Contributors:
-//    
-//    
+//    Duc Nguyen        dqn150030
+//    Luke Pettinger    flp081000
 // Date: 04/23/2017
 // Purpose: CS3376
 // Description:
 // 	This program initiates a UDP log server. Requires server_functions.c.
 
+int main(int argc, char *argv[])
+{
+	//Make UDP Connection.
+  
+  //Receive struct in recvfrom in while loop.
+  
+  //Call appendLog(logMessage)
+  
+  //Reply to caller with buffer.
+  
+	return 0;
+}

6509d43999f09855a4a4376ee7327c8ce50f42f5
fritzOSU03 <fritzOSU03@hotmail.com>
2017-04-23 15:35:02 -0500

    Update log_s.c

--- 2nd_Del/log_s.c
+++ 2nd_Del/log_s.c
@@ -7,84 +7,76 @@
 // Description:
 // 	This program initiates a UDP log server. Requires server_functions.c.
 
-#include <errno.h>
-#include <math.h>
-#include <netdb.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <sys/select.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <netdb.h>
+#include <stdio.h>
 #include "server_functions.h"
+
 
 int main(int argc, char *argv[])
 {
-	//Define variables
-	int udpfd, i, len;
-	ssize_t n;
+	int udpfd, n;
 	socklen_t clilen;
-	struct sockaddr_in cli_addr, serv_addr;
-	struct logMessage message;
-	//struct tm * timeinfo;
-	char buffer[1024];
+	struct sockaddr_in serv_addr, cli_addr;
+	struct logMessage logMsg;
 	
 	//Set serv_addr to all zeros.
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-
+	bzero(&serv_addr, sizeof(serv_addr));
+	
 	//Create the socket.
 	udpfd = setupSocket(SOCK_DGRAM, serv_addr, 9999);
 	if(udpfd < 0) error("ERROR opening socket ");
-
+	
 	//Bind the socket to the address.
 	if(bind(udpfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
-	error("ERROR on binding ");
-
+		error("ERROR on binding ");
+	
 	//Get the size of the client address structure.
 	clilen = sizeof(cli_addr);
 	
 	//Loop infinitely to receive connections.
 	while(1) {
 		//Set message to all zeros.
-		bzero(buffer, 1024);
+		bzero(&logMsg, sizeof(struct logMessage));
 		
-		//Receive the logMessage from the client.
-		n = recvfrom(udpfd, buffer, 1024, 0, (struct sockaddr *)&cli_addr, &clilen);
+		//Receive the information from the client.
+		n = recvfrom(udpfd, &logMsg, sizeof(struct logMessage), 0, (struct sockaddr *)&cli_addr, &clilen);
 		if(n < 0) error("ERROR receiving from socket ");
-		memcpy(&message, buffer, sizeof message);
-
-		//Convert logMessage.time to a tm struct.
-		//time ( &message.dateTime);
-		//timeinfo = localtime ( &message.dateTime);
-
-
-		//Find and clear the line return from logMessage.message.
-		i = 0;
-		len = strlen(message.message)+1;
-		for(i=0; i<len; i++)
-		{
-			if(message.message[i] == '\n')
-			{
-				//Shift characters 1 to left.
-				strncpy(&message.message[i],&message.message[i+1],len-i);
-			}
+		
+		//Convert the given time to a tm struct.
+		struct tm * now = localtime(&logMsg.dateTime);
+		
+		//Find and clear the line return.
+		for(int i = 0; (unsigned) i < (size_t) sizeof(logMsg.message); i++) {
+			if(logMsg.message[i] == 10) logMsg.message[i] = 0;
 		}
-
+		
 		//Print the message.
-		printf("%s\n",message.message);
-
+		printf(
+			"Logging: %.4d-%.2d-%.2d %.2d:%.2d:%.2d	\"%s\" was received from %d.%d.%d.%d\n",
+			(now->tm_year + 1900),
+			(now->tm_mon + 1),
+			now->tm_mday,
+			now->tm_hour,
+			now->tm_min,
+			now->tm_sec,
+			logMsg.message,
+			int(logMsg.address&0xFF),
+			int((logMsg.address&0xFF00)>>8),
+			int((logMsg.address&0xFF0000)>>16),
+			int((logMsg.address&0xFF000000)>>24));
 		
 		//Write to the log file.
-		appendLog(message);
+		appendLog(logMsg);
 		
 		//Write the response to the client.
-		n = sendto(udpfd, (const struct logMessage *)&message.message, sizeof(struct logMessage), 0, (struct sockaddr *)&cli_addr, clilen);
+		n = sendto(udpfd, "Log file updated.", 17, 0, (struct sockaddr *)&cli_addr, clilen);
 		if(n < 0) error("ERROR sending to socket ");
-		
 	}
 	
 	return 0;
